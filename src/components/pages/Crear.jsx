e11f6e42e7395180ea8bdf513baf3d87d85fd428
Aliesky Perez <aliesky.perez@gmail.com>
2023-06-09 09:01:22 -0400

    solved validation form create

--- src/components/pages/Crear.jsx
+++ src/components/pages/Crear.jsx
@@ -1,21 +1,15 @@
 import { useState } from "react";
-//import { Peticion } from "../../helpers/Peticion";
-import { Global } from "../../helpers/Global";
-
-//import { useDispatch } from "react-redux"
-//import {addNewArticle} from "../../store/slices/articulos/articleActions"
-import axios from "axios";
+import { useDispatch } from "react-redux";
+import { addNewArticle } from "../../store/slices/articulos/articleActions";
 
 export const Crear = () => {
-  ///OLD
-  //NEW
+  const dispatch = useDispatch();
   const [formState, setFormState] = useState({
     titulo: "",
     contenido: "",
   });
+  const [errors, setErrors] = useState({});
   const [file, setFile] = useState(null);
-
-  // const dispatch = useDispatch()
 
   const handleFileChange = ({ target }) => {
     setFile(target.files[0]);
@@ -28,42 +22,27 @@ export const Crear = () => {
     }));
   };
 
+  const validatorForm = (objeto) => {
+    const error = {};
+
+    !objeto.titulo && (error.titulo = "falta el campo titulo");
+    !objeto.contenido && (error.contenido = "falta el campo contenido");
+
+    return error;
+  };
+
   const guardarArticulo = async (e) => {
     e.preventDefault();
 
-    const formData = new FormData();
-    
-    formData.append("file0", file);
-    formData.append("data", JSON.stringify(formState));
+    setErrors(validatorForm(formState));
+    if (!Object.keys(errors)) {
+      const formData = new FormData();
 
-    axios.post(Global.url + "crear", formData, {
-      headers: {
-        "Content-Type": "multipart/form-data",
-      },
-    });
+      formData.append("file0", file);
+      formData.append("data", JSON.stringify(formState));
 
-    //const data = dispatch(addNewArticle(formState))
-
-    //console.log(data)
-    /* if (file) {
-        const formData = new FormData();
-        formData.append('file0',file);
-
-        axios.post("")
-
-        const subida = await Peticion(
-        Global.url + "subir-imagen/" + data._id,
-        "POST",
-        formData,
-        true
-      );
-
-      if (subida.datos.status === "success") {
-        setResultado("guardado");
-      } else {
-        setResultado("error");
-      }
-    }  */
+      dispatch(addNewArticle(formState));
+    }
   };
 
   return (
@@ -76,24 +55,24 @@ export const Crear = () => {
  */}
       <form className='formulario' onSubmit={guardarArticulo}>
         <div className='form-group'>
-          <label htmlFor='titulo'>Title</label>
+          <label htmlFor='titulo'>Title: <span style={{color:"red", fontSize:10}}>{errors.titulo}</span></label>
           <input
             type='text'
-            required
             name='titulo'
             onChange={handleChange}
             value={formState.titulo}
           />
+          
         </div>
 
         <div className='form-group'>
-          <label htmlFor='contenido'>Content</label>
+        <label htmlFor='titulo'>Contenido: <span style={{color:"red", fontSize:10}}>{errors.contenido}</span></label>
           <textarea
             type='text'
-            required
             name='contenido'
             onChange={handleChange}
             value={formState.contenido}
+            placeholder="Contenido"
           />
         </div>
 
@@ -101,7 +80,6 @@ export const Crear = () => {
           <label htmlFor='file0'>Image</label>
           <input
             type='file'
-            required
             name='file0'
             id='file'
             onChange={handleFileChange}
--- src/store/slices/articulos/articleCRUD.js
+++ src/store/slices/articulos/articleCRUD.js
@@ -1,12 +1,14 @@
 import { Global } from "../../../helpers/Global";
 import axios from "axios";
 
-export const  getAllArticlesAPI = async () => {
+export const getAllArticlesAPI = async () => {
   return await axios.get(Global.url + "articulos");
 };
 
-export const addArticleAPI = async (article) =>{
-  return await axios.post(Global.url + "crear", article)
-}
-
-
+export const addArticleAPI = async (article) => {
+  return await axios.post(Global.url + "crear", article, {
+    headers: {
+      "Content-Type": "multipart/form-data",
+    },
+  });
+};
